37581c8f6a09b6d189c242d8399824333376666e
Lord225 <zlotymaciej@gmail.com>
2023-05-18 12:40:28 +0200

    nn

--- src/chess_lr_3_real_action.py
+++ src/chess_lr_3_real_action.py
@@ -6,10 +6,18 @@ import tensorflow as tf
 import tqdm
 import config_file
 import tensorboard
-
+from collections import deque
 from reinforce.data_collector import run_episode_and_get_history_4
 from reinforce.replay_memory import ReplayMemory
 from reinforce.train import training_step_dqnet_target_critic, training_step_no_critic_no_target
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--resume", type=str, default=None, help="resume from a model") 
+
+args = parser.parse_args()
+
 env = chess_engine.DiagonalChess()
 n_outputs = 4096
 
@@ -21,7 +29,7 @@ def env_step(action):
     state2, reward2, done2 = env.step(int(random_action))
 
     state = state2
-    reward = reward1 #- reward2
+    reward = reward1 - max(reward2, 0)
     done = done1 or done2
 
     return (state.astype(np.float32), np.array(reward, np.float32), np.array(done, np.int32))
@@ -41,19 +49,30 @@ def tf_random_legal_action():
     return tf.numpy_function(random_legal_action, [], tf.int32) #type: ignore
 
 
-def get_network():
+def get_network() -> tf.keras.Model:
+    if args.resume is not None:
+        model = tf.keras.models.load_model(config_file.MODELS_DIR + args.resume)
+        print("loaded model from", config_file.MODELS_DIR + args.resume)
+        return model # type: ignore
+    
     inputs = tf.keras.Input(shape=(8, 8, 8))
-    x = tf.keras.layers.Conv2D(32,  3, activation="relu", padding='same')(inputs)
-    x = tf.keras.layers.Conv2D(64,  3, activation="relu", padding='same')(x)
-    x = tf.keras.layers.Conv2D(128, 2, activation="relu", padding='same')(x)
-    x = tf.keras.layers.Conv2D(128,  2, activation="relu", padding='same')(x)
-    x = tf.keras.layers.Conv2D(64,  2, activation="relu", padding='same')(x)
+    x = tf.keras.layers.Conv2D(32,  3, activation="elu", padding='same')(inputs)
+    x = tf.keras.layers.Conv2D(64,  3, padding='same', activation="elu")(x)
+    x = tf.keras.layers.Conv2D(128,  2, padding='same', activation="elu")(x)
+    x = tf.keras.layers.Conv2D(256,  2, padding='same', activation="elu")(x)
+    x = tf.keras.layers.Conv2D(256,  2, padding='same', activation="elu")(x)
+    x = tf.keras.layers.Conv2D(256,  2, padding='same', activation="elu")(x)
+    x = tf.keras.layers.Conv2D(128,  2, padding='same', activation="elu")(x)
+    x = tf.keras.layers.Conv2D(64,  4, padding='same', activation="elu")(x)
+    x = tf.keras.layers.Conv2D(64,  4, padding='same', activation="elu")(x)
     actor = tf.keras.layers.Flatten()(x)
     critic = tf.keras.layers.Flatten()(x)
     critic = tf.keras.layers.Dense(2, activation="relu")(critic)
     critic = tf.keras.layers.Dense(1)(critic)
 
     model = tf.keras.Model(inputs=inputs, outputs=[actor, critic])
+
+    model.compile()
 
     print(model.summary())
 
@@ -65,27 +84,42 @@ target_model.set_weights(actor_model.get_weights())
 
 RUN_VERSION = "v4.0"
 
+print("run:", config_file.LOG_DIR+RUN_VERSION)
 train_summary_writer = tf.summary.create_file_writer(config_file.LOG_DIR+RUN_VERSION) #type: ignore
 
-batch_size = 32
-discount_rate = 0.99
-episodes = 20000
-train_iters_per_episode = 1
-max_steps_per_episode = 10
+batch_size = 1000
+discount_rate = 0.5
+episodes = 200000
+train_iters_per_episode = 50
+max_steps_per_episode = 15
 target_update_freq = 100
-minibatch_size = 32
-replay_memory_size = 4000
+minibatch_size = 64
+replay_memory_size = 3000
 save_freq = 250
-lr = 3e-2
 
-optimizer = tf.keras.optimizers.Adam(learning_rate=lr)
+eps_decay_len = 1000
+eps_min = 0.1
+
+lr = 2.5e-4
+
+optimizer = tf.keras.optimizers.RMSprop(
+    learning_rate=lr,
+    rho=0.95,
+    momentum=0.0,
+    epsilon=1e-07,
+    centered=True,
+)
+
+# optimizer = tf.keras.optimizers.Adam(learning_rate=lr)
+
+running_avg = deque(maxlen=500)
 
 def run():
     replay_memory = ReplayMemory(replay_memory_size)
 
     t = tqdm.tqdm(range(episodes))
     for episode in t:
-        epsilon = max(1 - episode / 1000, 0.01)
+        epsilon = max(1 - episode / eps_decay_len, eps_min)
 
         # run episode
         state = env.reset()
@@ -95,38 +129,38 @@ def run():
                                                                                                            max_steps_per_episode, 
                                                                                                            discount_rate,
                                                                                                            epsilon,
-                                                                                                           tf_env_step,
-                                                                                                           tf_random_legal_action)  # type: ignore
+                                                                                                           tf_env_step
+                                                                                                           )  # type: ignore
 
         # add to replay memory
         replay_memory.add(states, action_probs, returns, next_states, dones)
+        running_avg.append(total_rewards)
+        avg = sum(running_avg)/len(running_avg)
+
+
 
         # log
         with train_summary_writer.as_default():
             tf.summary.scalar('reward', total_rewards, step=episode)
+            tf.summary.scalar('reward_avg', avg, step=episode)
             tf.summary.scalar('lenght', states.shape[0], step=episode)
 
-        t.set_description(f"Episode {episode} - Reward: {total_rewards:.2f}")
+        t.set_description(f"Episode {episode} - Reward: {total_rewards:.2f} - Avg: {avg:.2f}")
         
         #train
         if len(replay_memory) > batch_size+1:
             batch = replay_memory.sample(batch_size)
     
-            # training_step_dqnet_target_critic(batch, 
-            #                                   minibatch_size, 
-            #                                   train_iters_per_episode, 
-            #                                   discount_rate, 
-            #                                   target_model, 
-            #                                   actor_model, 
-            #                                   optimizer,
-            #                                   n_outputs,
-            #                                   )
-            training_step_no_critic_no_target(batch,
-                                              actor_model,
+            training_step_dqnet_target_critic(batch, 
+                                              minibatch_size, 
+                                              train_iters_per_episode, 
+                                              discount_rate, 
+                                              target_model, 
+                                              actor_model, 
                                               optimizer,
                                               n_outputs,
-                                              discount_rate
-            )
+                                              )
+            
         # update target network
         if episode % target_update_freq == 0:
             target_model.set_weights(actor_model.get_weights())
